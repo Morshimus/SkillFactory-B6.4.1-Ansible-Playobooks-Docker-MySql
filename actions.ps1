a507889a3941fd8daa5d616eb7c940b2fd3dee07
Daniel Dalavurak <morsh110792@yahoo.com>
2023-01-30 20:29:14 +0300

    ok

--- actions.ps1
+++ actions.ps1
@@ -22,3 +22,18 @@ function ansiblePlaybook {
     )
     wsl -d $distr -u $user -e ansible-playbook $args
 } 
+
+function createWslMySQLDockerContainer {
+    param (
+        [Parameter(Mandatory=$true,Position=0)]
+        [string]$user,
+        [Parameter(Mandatory=$false,Position=1)]
+        [Securestring]$dbPassword = $(Read-Host 'What is your password?' -AsSecureString)
+    )
+    
+    $path = $($(Get-Location).path -replace "\\", "/") -replace "C:","/mnt/c"
+    
+    wsl -d Ubuntu-20.04 -u $user -e ansible-playbook --extra-vars 'local_wsl_user=$user password_db=$dbPassword '  $path/provisioning.yaml
+
+    wsl -d Ubuntu-20.04 -u $user -e docker exec -ti 
+}
--- docker-postconfig.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DOCKER_DIR=/mnt/wsl/shared-docker && \
-sudo mkdir -pm o=,ug=rwx "$DOCKER_DIR" && \
-sudo chgrp docker "$DOCKER_DIR" 
-
